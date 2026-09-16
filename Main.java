24a5d20fc05f69d810cc05863279b5f0faa96035
MarcoCastellini <marco.castellini@marconirovereto.it>
2026-09-16 12:29:07 +0000

    Refactor code structure for improved readability and maintainability

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
 
@@ -9,20 +10,25 @@ public class Main {
         try {
             // Crea un riferimento al file da leggere.
             File file = new File("file.txt");
+            File output = new File("output.txt");
             // Apre il file per poterlo leggere con Scanner.
             Scanner scanner = new Scanner(file);
+            PrintWriter writer = new PrintWriter(output);
             String riga;
             String[] parole;
             // Legge e stampa ogni riga fino alla fine del file.
             while (scanner.hasNextLine()) {
                 riga = scanner.nextLine();
-                parole = riga.split(" ");
+                parole = riga.split("[ ;.,:?!']");
                 for (int i = 0; i < parole.length; i++) {
-                    System.out.println(parole[i]);
+                    if (parole[i] != "") {
+                        writer.println(parole[i]);
+                    }
                 }
             }
             // Chiude lo Scanner e libera la risorsa associata al file.
             scanner.close();
+            writer.close();
         } catch (FileNotFoundException e) {
             // Mostra un messaggio se il file non esiste o non è accessibile.
             System.out.println(e.getMessage());
