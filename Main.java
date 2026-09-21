ac0fa0e0634f3b60fa0b649dc5cbd85acd9809df
MarcoCastellini <marco.castellini@marconirovereto.it>
2026-09-21 09:50:43 +0000

    Aggiungi la classe Parola con gestione del conteggio e ordinamento delle parole

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-
     public static void main(String[] args) {
 
         try {
@@ -16,19 +17,39 @@ public class Main {
             PrintWriter writer = new PrintWriter(output);
             String riga;
             String[] parole;
+            boolean trovato = false;
             String parola;
+            // dichiaro un ArrayList di parola
+            ArrayList<Parola> lista = new ArrayList<>();
+
             // Legge e stampa ogni riga fino alla fine del file.
             while (scanner.hasNextLine()) {
                 riga = scanner.nextLine();
                 parole = riga.split("[ ;.,:?!']");
                 for (int i = 0; i < parole.length; i++) {
                     parola = parole[i].toLowerCase().trim();
-                    if (!parola.equals(" ") && parola.length()>3) {
+                    if (!parola.equals(" ") && parola.length() > 3) {
                         writer.println(parola);
+                        // se non è presente
+                        for (Parola parolaInLista : lista) {
+                            trovato = false;
+                            if (parola.equals(parolaInLista.getTesto())) {
+                                parolaInLista.incrementa();
+                                trovato = true;
+                                break;
+                            }
+                        }
+                        if (!trovato) {
+                            // aggiunge in coda alla lista
+                            lista.add(new Parola(parola));
+                        }
+
                     }
                 }
             }
             // Chiude lo Scanner e libera la risorsa associata al file.
+            Collections.sort(lista);
+            System.out.println(lista);
             scanner.close();
             writer.close();
         } catch (FileNotFoundException e) {
--- Parola.java
+++ Parola.java
@@ -1,7 +1,33 @@
-public class Parola{
-    String testo;
-    int contatore = 1;
-    Parola(String testo){
+public class Parola implements Comparable<Parola> {
+
+    private String testo;
+    private int contatore = 1;
+
+    public Parola(String testo, int contatore){
         this.testo = testo;
+        this.contatore = contatore;
     }
+
+    public Parola(String testo){
+        this(testo, 1);
+    }
+
+    public void incrementa() {
+        this.contatore++;
+    }
+
+    public String getTesto(){
+        return testo;
+    }
+
+    @Override 
+    public String toString(){
+        return this.testo + ": " + this.contatore;
+    }
+
+    @Override
+    public int compareTo(Parola o) {
+        return o.contatore - this.contatore;
+    }
+
 }
